cf2b7077746830284b73ade47e2ec969d7425998
abdinoor <abdinurkune@gmail.com>
2025-02-01 15:36:30 +0300

    cgffgg

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
 import { CartContext } from "./ContextProvider";
+import Cart from "./cart";
+
 
 const Product = ({ product }) => {
   const{dispatch } = useContext(CartContext)
