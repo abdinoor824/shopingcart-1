29a738f9317131494752fe31a416abf2e8d44e71
abdinoor <abdinurkune@gmail.com>
2025-02-01 15:56:18 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import Cart from "./components/Cart"
+
+import Cart from "./components/cart"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Products from "./components/products"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+
 
 function App() {
  
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { CartContext } from "./ContextProvider";
-import Cart from "./Cart";
+
 
 
 const Product = ({ product }) => {
