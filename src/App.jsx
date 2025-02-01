9573b28a46fba289a2426b634e2d102d04fae6c3
abdinoor <abdinurkune@gmail.com>
2025-02-01 20:48:48 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Cart from "./components/Cart"
+import Cart from "./components/cart"
 import Footer from "./components/Footer"
 import Navbar from "./components/Navbar"
 import Products from "./components/products"
