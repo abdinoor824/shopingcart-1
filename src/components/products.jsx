076b8f33c5f0b4cc86d10528681d5ed0065add4c
abdinoor <abdinurkune@gmail.com>
2025-02-02 11:39:43 +0300

    last commit

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Data from '../../Data.json';
 import Product from './product';
 
+
 const Products = () => {
   const [products, setProducts] = useState(Data.products);
 
@@ -9,7 +10,10 @@ const Products = () => {
     
    
     
-    <div className='grid md:grid-cols-3 my-20 overflow-hidden font-medium '>
+    <div className='grid md:grid-cols-3 my-20 overflow-hidden font-medium  bg-cover bg-gray-100 bg-center bg-no-repeat bg-fixed '
+    
+    >
+
       {products.map(p => (
         <Product key={p.id} product={p} />
       ))}
